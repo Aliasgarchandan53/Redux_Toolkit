b9454d9f43deb072a6634129e9d4b5576188f637
Aliasgarchandan53 <aliasgarchandan53@gmail.com>
2024-04-19 11:43:36 +0530

    Finished todo

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Steps to setup Redux :
+- Step 1 - Create and configure store in a (separate  folder-suggested)
+- Step 2 - Create reducer for store
+- Step 3 - Reducer consist of initial state and a list of reducer functions
+- Step 4 - Update configured reducer in store
---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos:[{id:1, text:"hey there !"}] // array of todos
+}
+
+export const todoSlice=createSlice({
+    name:'todo', // name is property from redux to identify the slice
+    initialState, //initial state is passed
+    reducers:{      //consists of properties and functions
+        addTodo:(state,action)=>{
+            const todo ={
+                id:nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo)
+        },  //state and action are two properties we always get access to in reducer functions
+        removeTodo:(state,action)=>{
+            state.todos=state.todos.filter((todo)=>{
+                        todo.id!==action.payload
+            })
+        }
+    }
+})
+
+export const {addTodo,removeTodo} = todoSlice.actions //to be used in components
+
+export default todoSlice.reducer // for awareness of store  
+
+/*  
+    State - this varible gives access to current state of  data
+    action - this varible gives reference to certain values in state to act upon data eg - giving ids to remove todo
+*/
